5e0c9fc6db3b266e0cbfed7b99077318e6a3da15
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-04-02 22:55:34 +0530

    different menu item

--- app/src/main/java/com/example/p7_menus/MainActivity.kt
+++ app/src/main/java/com/example/p7_menus/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.p7_menus
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,8 +39,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import com.example.p7_menus.ui.theme.P7menusTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,51 +55,115 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    MyApp()
                 }
+            }
+        }
+    }
+}
+@Composable
+fun MyApp() {
+    Surface {
+        Column {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween) {
+            ContextMenu()
+            AppBarMenu()
+            }
+            Column(modifier = Modifier
+                .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+            PopUpMenu()
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBarMenu() {
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("appBar 1", "appBar 2", "appBar 3")
+    val context = LocalContext.current
+
+    TopAppBar(
+        title = { Text(text = "") },
+        actions = {
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(text = { Text(text = option) }, onClick = {
+                        Toast.makeText(context, "Pressed $option", Toast.LENGTH_SHORT).show()
+                        expanded = false
+                         })
+                }
+            }
+        }
+    )
+}
+@Composable
+fun ContextMenu() {
+    val context = LocalContext.current
+    val options = listOf("context 1", "context 2", "context 3")
+    var expanded by remember { mutableStateOf(false) }
+
+    Column {
+        IconButton(
+            onClick = { expanded = !expanded },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(Icons.Default.Menu, contentDescription = "Menu")
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(text = { Text(text = option) }, onClick = {
+                    Toast.makeText(context, "Pressed $option", Toast.LENGTH_SHORT).show()
+                    expanded = false
+                })
             }
         }
     }
 }
 
 @Composable
-fun MainScreen() {
-    var isConExp by remember {
-        mutableStateOf(false)
-    }
-    var isAppExp by remember {
-        mutableStateOf(false)
-    }
+fun PopUpMenu() {
+    val context = LocalContext.current
+    val options = listOf("popUp 1", "popUp 2", "popUp 3")
+    var expanded by remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black)) {
-
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .background(Color.Black)
-        .padding(5.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween) {
-        IconButton(onClick = { isConExp = true }) {
-            androidx.compose.material3.Icon(imageVector = Icons.Filled.Menu, contentDescription = "context", tint = Color.White)
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            androidx.compose.material3.Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "app bar", tint = Color.White)
-        }
-    }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-
-        IconButton(onClick = { isAppExp = true }) {
-        androidx.compose.material3.Icon(imageVector = Icons.Filled.AddCircle, contentDescription = "pop up", tint = Color.White,
-            modifier = Modifier.size(90.dp))
+    Column {
+        IconButton(
+            onClick = { expanded = !expanded },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(Icons.Default.AddCircle, contentDescription = "Menu",Modifier.size(60.dp))
         }
 
-    }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(text = { Text(text = option) }, onClick = {
+                    Toast.makeText(context, "Pressed $option", Toast.LENGTH_SHORT).show()
+                    expanded = false
+                })
+            }
+        }
     }
 }
 
@@ -92,6 +171,6 @@ fun MainScreen() {
 @Composable
 fun GreetingPreview() {
     P7menusTheme {
-        MainScreen()
+        MyApp()
     }
 }
